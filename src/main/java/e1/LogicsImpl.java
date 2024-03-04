3c9e98d9b59d0b5548c606aace240085fca943f6
Andrea Biagini <andrea.biagini5@studio.unibo.it>
2024-03-04 19:07:02 +0100

    Step 1

--- src/main/java/e1/LogicsImpl.java
+++ src/main/java/e1/LogicsImpl.java
@@ -4,17 +4,30 @@ import java.util.*;
 
 public class LogicsImpl implements Logics {
 	
-	private final Pair<Integer,Integer> pawn;
+	private Pair<Integer,Integer> pawn;
 	private Pair<Integer,Integer> knight;
 	private final Random random = new Random();
-	private final int size;
+	private int size;
+
+	public LogicsImpl(int size, Pair<Integer, Integer> pawnPosition, Pair<Integer, Integer> knightPosition) {
+		this.setSize(size);
+		this.pawn = new Pair<Integer,Integer>(pawnPosition.getX(), pawnPosition.getY());
+		this.knight = new Pair<Integer,Integer>(knightPosition.getX(), knightPosition.getY());
+	}
 	 
-    public LogicsImpl(int size){
-    	this.size = size;
+    public LogicsImpl(int size) {
+		this.setSize(size);
         this.pawn = this.randomEmptyPosition();
         this.knight = this.randomEmptyPosition();	
     }
     
+	private void setSize(int size) {
+		if (size == 1) {
+			throw new IllegalArgumentException("Size must be greater than 1.");
+		}
+    	this.size = size;
+	}
+
 	private final Pair<Integer,Integer> randomEmptyPosition(){
     	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
     	// the recursive call below prevents clash with an existing pawn
@@ -24,12 +37,12 @@ public class LogicsImpl implements Logics {
 	@Override
 	public boolean hit(int row, int col) {
 		if (row<0 || col<0 || row >= this.size || col >= this.size) {
-			throw new IndexOutOfBoundsException();
+			throw new IndexOutOfBoundsException("Position must be inside of the board.");
 		}
 		// Below a compact way to express allowed moves for the knight
-		int x = row-this.knight.getX();
-		int y = col-this.knight.getY();
-		if (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3) {
+		int deltaX = row - this.knight.getX();
+		int deltaY = col - this.knight.getY();
+		if (deltaX != 0 && deltaY != 0 && Math.abs(deltaX) + Math.abs(deltaY) == 3) {
 			this.knight = new Pair<>(row,col);
 			return this.pawn.equals(this.knight);
 		}
--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,90 @@
 package e1;
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+    private Logics logics;
+    private final int size = 5;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.logics = new LogicsImpl(this.size);
+    }
+    
+    @Test
+    public void testInstantiation() {
+        assertNotNull(this.logics);
+    }
+
+    @Test
+    public void testKnightPresence() {
+        var knightPosition = this.getKnightPosition();
+        assertAll(
+            () -> assertNotEquals(-1, knightPosition.getX()),
+            () -> assertNotEquals(-1, knightPosition.getY())
+        );
+    }
+
+    @Test
+    public void testPawnPresence() {
+        var pawnPosition = this.getPawnPosition();
+        assertAll(
+            () -> assertNotEquals(-1, pawnPosition.getX()),
+            () -> assertNotEquals(-1, pawnPosition.getY())
+        );
+    }
+
+    private Pair<Integer, Integer> getKnightPosition() {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                if (this.logics.hasKnight(i, j)) {
+                    return new Pair<>(i, j);
+                }
+            }
+        }
+        return new Pair<>(-1, -1);
+    }
+
+    private Pair<Integer, Integer> getPawnPosition() {
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                if (this.logics.hasPawn(i, j)) {
+                    return new Pair<>(i, j);
+                }
+            }
+        }
+        return new Pair<>(-1, -1);
+    }
+
+    @Test
+    public void testPiecesNotOverlap() {
+        var knightPosition = this.getKnightPosition();
+        var pawnPosition = this.getPawnPosition();
+        assertFalse(
+            knightPosition.getX() == pawnPosition.getX() && 
+            knightPosition.getY() == pawnPosition.getY()
+        );
+    }
+
+    // Tests on the hit function are missing
+
+    // Additional tests after small additions to LogicsImpl
+
+    @Test
+    public void testInstantnationPassingPiecesPositions() {
+        var pawnPosition = new Pair<>(0, (int) this.size / 2);
+        var knightPosition = new Pair<>((int) this.size / 2, 0);
+        var newLogics = new LogicsImpl(this.size, pawnPosition, knightPosition);
+        assertNotNull(newLogics);
+    }
+
+    @Test
+    public void testWrongSizeInstantiation() {
+       assertThrows(IllegalArgumentException.class, () -> {
+        var wrongLogic = new LogicsImpl(1);
+        wrongLogic.hasKnight(0, 0);
+       }); 
+    }
 }
